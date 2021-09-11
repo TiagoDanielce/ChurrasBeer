20bbdaf0f895ea06ef50fba2263d8267cd00a068
Tiago Danielce <tiagorodrigues_svp@hotmail.com>
2021-09-10 21:09:00 -0300

    create decimal extensions

---
+++ src/ChurrasBeer.Infrastructure.CrossCutting/Extensions/Decimals/DecimalExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace ChurrasBeer.Infrastructure.CrossCutting.Extensions.Decimals
+{
+    public static class DecimalExtension
+    {
+        public static bool IsBetween(this decimal? value, decimal minValue, decimal maxValue)
+        {
+            if (value == null)
+                return false;
+
+            return value.Value.IsBetween(minValue, maxValue);
+        }
+
+        public static bool IsBetween(this decimal value, decimal minValue, decimal maxValue)
+        {
+            return value >= minValue && value <= maxValue;
+        }
+    }
+}
