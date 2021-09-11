92c9141e260162ee2f3c73acb92540cb71cd0557
Tiago Danielce <tiagorodrigues_svp@hotmail.com>
2021-09-10 21:10:24 -0300

    create enum extensions

---
+++ src/ChurrasBeer.Infrastructure.CrossCutting/Extensions/Enums/EnumExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace ChurrasBeer.Infrastructure.CrossCutting.Extensions.Enums
+{
+    public static class EnumExtension
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            if (enumValue == null)
+                return string.Empty;
+
+            return enumValue.GetType()?
+                            .GetMember(enumValue.ToString())?
+                            .First()?
+                            .GetCustomAttribute<DisplayNameAttribute>()?
+                            .DisplayName;
+        }
+    }
+}
