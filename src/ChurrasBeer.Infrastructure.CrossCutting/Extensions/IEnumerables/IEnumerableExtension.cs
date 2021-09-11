a3df4603668ee62a3621de5d02a305b7103b87fd
Tiago Danielce <tiagorodrigues_svp@hotmail.com>
2021-09-10 21:12:31 -0300

    create ienumerable extension

---
+++ src/ChurrasBeer.Infrastructure.CrossCutting/Extensions/IEnumerables/IEnumerableExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChurrasBeer.Infrastructure.CrossCutting.Extensions.IEnumerables
+{
+    public static class IEnumerableExtension
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+
+        public static IEnumerable<List<T>> Split<T>(this IEnumerable<T> enumerable, int nSize)
+        {
+            var list = enumerable.ToList();
+
+            for (int i = 0; i < list.Count; i += nSize)
+            {
+                yield return list.GetRange(i, Math.Min(nSize, list.Count - i));
+            }
+        }
+
+        public static IEnumerable<TSource> WhereIf<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, bool condition)
+        {
+            if (condition)
+                return source.Where(predicate);
+
+            return source;
+        }
+    }
+}
