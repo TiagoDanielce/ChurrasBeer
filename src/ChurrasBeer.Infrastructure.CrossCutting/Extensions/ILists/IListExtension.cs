d480ccdde545885ee1822125c3715a83786ee2ed
Tiago Danielce <tiagorodrigues_svp@hotmail.com>
2021-09-10 21:15:21 -0300

    create IList extension

---
+++ src/ChurrasBeer.Infrastructure.CrossCutting/Extensions/ILists/IListExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChurrasBeer.Infrastructure.CrossCutting.Extensions.ILists
+{
+    public static class IListExtension
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int count = list.Count;
+            while (count > 1)
+            {
+                count--;
+                int random = rng.Next(count + 1);
+                T value = list[random];
+                list[random] = list[count];
+                list[count] = value;
+            }
+        }
+    }
+}
