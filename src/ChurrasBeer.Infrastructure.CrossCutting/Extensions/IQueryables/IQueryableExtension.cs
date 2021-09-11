481bc7b409961bd41b795473dab261fb7fdf238b
Tiago Danielce <tiagorodrigues_svp@hotmail.com>
2021-09-10 21:17:02 -0300

    create iqueryable extension

---
+++ src/ChurrasBeer.Infrastructure.CrossCutting/Extensions/IQueryables/IQueryableExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ChurrasBeer.Infrastructure.CrossCutting.Extensions.IQueryables
+{
+    public static class IQueryableExtension
+    {
+        public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, bool>> predicate, bool condition)
+        {
+            if (condition)
+                return source.Where(predicate);
+
+            return source;
+        }
+    }
+}
