41b48fe79bbe3e6ab9bcf839e9230f1774883aac
Tiago Danielce <tiagorodrigues_svp@hotmail.com>
2021-09-10 21:11:24 -0300

    create icollection extensions

---
+++ src/ChurrasBeer.Infrastructure.CrossCutting/Extensions/ICollections/ICollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ChurrasBeer.Infrastructure.CrossCutting.Extensions.ICollections
+{
+    public static class ICollectionExtension
+    {
+        public static bool IsNullOrEmpty<T>(this ICollection<T> collection)
+        {
+            return collection == null || !collection.Any();
+        }
+    }
+}
